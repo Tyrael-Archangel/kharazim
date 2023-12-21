4645f32f16f79b66734d5d5da5adf312d996749e
Tyrael-Archangel <tyrael_archangel@163.com>
2023-12-21 23:59:05 +0800

    add BaseNameAndValueEnum.

---
+++ kharazim-application/src/main/java/com/tyrael/kharazim/common/dto/BaseNameAndValueEnum.java
@@ -0,0 +1,23 @@
+package com.tyrael.kharazim.common.dto;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2023/12/21
+ */
+public interface BaseNameAndValueEnum {
+
+    /**
+     * get value
+     *
+     * @return value
+     */
+    Integer getValue();
+
+    /**
+     * get name
+     *
+     * @return name
+     */
+    String getName();
+
+}
