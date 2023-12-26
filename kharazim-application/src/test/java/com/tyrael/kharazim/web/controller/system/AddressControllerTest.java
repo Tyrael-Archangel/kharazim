5928e81ca1e1fdcaea4e20d185062e4b07d3afdd
Tyrael-Archangel <tyrael_archangel@163.com>
2023-12-26 10:54:32 +0800

    feature: address test and sql script.

---
+++ kharazim-application/src/test/java/com/tyrael/kharazim/web/controller/system/AddressControllerTest.java
@@ -0,0 +1,48 @@
+package com.tyrael.kharazim.web.controller.system;
+
+import com.tyrael.kharazim.web.controller.BaseControllerTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2023/12/26
+ */
+class AddressControllerTest extends BaseControllerTest<AddressController> {
+
+    AddressControllerTest() {
+        super(AddressController.class);
+    }
+
+    @Test
+    void fullTree() {
+        super.performWhenCall(mockController.fullTree());
+    }
+
+    @Test
+    void treeByRootId() {
+        super.performWhenCall(mockController.treeByRootId(2L));
+    }
+
+    @Test
+    void queryByName() {
+        super.performWhenCall(mockController.queryByName("北京市"));
+    }
+
+    @Test
+    void listProvince() {
+        super.performWhenCall(mockController.listProvince());
+    }
+
+    @Test
+    void queryCities() {
+        // 四川省
+        super.performWhenCall(mockController.queryCities("23"));
+    }
+
+    @Test
+    void queryCounties() {
+        // 四川省-成都市
+        super.performWhenCall(mockController.queryCounties("23-1"));
+    }
+
+}
