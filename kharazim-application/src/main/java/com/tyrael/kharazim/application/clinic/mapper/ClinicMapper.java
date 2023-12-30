8eb03ee567316f885999fdd2995b700bbbb3d9d9
Tyrael-Archangel <tyrael_archangel@163.com>
2023-12-30 17:23:10 +0800

    feature: clinic mapper

---
+++ kharazim-application/src/main/java/com/tyrael/kharazim/application/clinic/mapper/ClinicMapper.java
@@ -0,0 +1,13 @@
+package com.tyrael.kharazim.application.clinic.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tyrael.kharazim.application.clinic.domain.Clinic;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2023/12/30
+ */
+@Mapper
+public interface ClinicMapper extends BaseMapper<Clinic> {
+}
