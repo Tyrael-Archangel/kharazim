5bc2a6a3287b60af6f00c5a5b78d13ba2863ef75
Tyrael-Archangel <tyrael_archangel@163.com>
2024-01-08 13:49:03 +0800

    sql: with rollup demo.

---
+++ kharazim-demo/sql/20240108_with_rollup_demo.sql
@@ -0,0 +1,16 @@
+create table `demo_movies`
+(
+    `id`    bigint         not null auto_increment primary key,
+    `name`  varchar(32)    not null,
+    `price` decimal(10, 2) not null
+);
+
+insert into `demo_movies`(`name`, `price`)
+values ('Spider Man', 38.88),
+       ('Bat Man', 42.99),
+       ('Super Man', 51.98);
+
+select name, sum(price)
+from `demo_movies`
+group by name
+with rollup;
