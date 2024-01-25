84a6ac1b025fac59171d67d8708cf090a0d1c7ed
Tyrael-Archangel <tyrael_archangel@163.com>
2024-01-25 15:31:03 +0800

    add. RechargeCardType domain/serviceInterface/mapperInterface

---
+++ kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/RechargeCardTypeService.java
@@ -0,0 +1,8 @@
+package com.tyrael.kharazim.application.recharge.service;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2024/1/25
+ */
+public interface RechargeCardTypeService {
+}
---
+++ kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/impl/RechargeCardTypeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.tyrael.kharazim.application.recharge.service.impl;
+
+import com.tyrael.kharazim.application.recharge.service.RechargeCardTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2024/1/25
+ */
+@Service
+@RequiredArgsConstructor
+public class RechargeCardTypeServiceImpl implements RechargeCardTypeService {
+}
