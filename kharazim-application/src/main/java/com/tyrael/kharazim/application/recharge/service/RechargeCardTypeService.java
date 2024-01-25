23ced14662142b1c3ed89369d425307a1c56bd3e
Tyrael-Archangel <tyrael_archangel@163.com>
2024-01-25 15:46:02 +0800

    feature: create RechargeCardType

--- kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/RechargeCardTypeService.java
+++ kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/RechargeCardTypeService.java
@@ -1,8 +1,19 @@
 package com.tyrael.kharazim.application.recharge.service;
 
+import com.tyrael.kharazim.application.recharge.vo.AddRechargeCardTypeRequest;
+
 /**
  * @author Tyrael Archangel
  * @since 2024/1/25
  */
 public interface RechargeCardTypeService {
+
+    /**
+     * 新建储值卡项
+     *
+     * @param addRequest {@link AddRechargeCardTypeRequest}
+     * @return 储值卡项编码
+     */
+    String create(AddRechargeCardTypeRequest addRequest);
+
 }
--- kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/impl/RechargeCardTypeServiceImpl.java
+++ kharazim-application/src/main/java/com/tyrael/kharazim/application/recharge/service/impl/RechargeCardTypeServiceImpl.java
@@ -1,8 +1,15 @@
 package com.tyrael.kharazim.application.recharge.service.impl;
 
+import com.tyrael.kharazim.application.config.BusinessCodeConstants;
+import com.tyrael.kharazim.application.recharge.domain.RechargeCardType;
+import com.tyrael.kharazim.application.recharge.mapper.RechargeCardTypeMapper;
 import com.tyrael.kharazim.application.recharge.service.RechargeCardTypeService;
+import com.tyrael.kharazim.application.recharge.vo.AddRechargeCardTypeRequest;
+import com.tyrael.kharazim.application.system.service.CodeGenerator;
+import com.tyrael.kharazim.common.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Tyrael Archangel
@@ -11,4 +18,36 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RechargeCardTypeServiceImpl implements RechargeCardTypeService {
+
+    private final RechargeCardTypeMapper rechargeCardTypeMapper;
+    private final CodeGenerator codeGenerator;
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public String create(AddRechargeCardTypeRequest addRequest) {
+
+        boolean neverExpire = Boolean.TRUE.equals(addRequest.getNeverExpire());
+        Integer validPeriodDays = addRequest.getValidPeriodDays();
+        if (neverExpire) {
+            validPeriodDays = null;
+        } else if (validPeriodDays == null) {
+            throw new BusinessException("请指定有效期限");
+        }
+
+        String code = codeGenerator.next(BusinessCodeConstants.RECHARGE_CARD_TYPE);
+
+        RechargeCardType cardType = new RechargeCardType();
+        cardType.setCode(code);
+        cardType.setName(addRequest.getName());
+        cardType.setDiscountPercent(addRequest.getDiscountPercent());
+        cardType.setNeverExpire(neverExpire);
+        cardType.setValidPeriodDays(validPeriodDays);
+        cardType.setDefaultAmount(addRequest.getDefaultAmount());
+        cardType.setCanCreateNewCard(Boolean.TRUE);
+
+        rechargeCardTypeMapper.insert(cardType);
+
+        return cardType.getCode();
+    }
+
 }
