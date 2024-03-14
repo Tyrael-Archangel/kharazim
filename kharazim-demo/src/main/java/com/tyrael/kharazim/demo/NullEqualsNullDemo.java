8840da7c448d345cca8faebe2b4b5cb739bf77d6
Tyrael-Archangel <tyrael_archangel@163.com>
2024-03-14 09:23:56 +0800

    demo NullEqualsNullDemo

---
+++ kharazim-demo/src/main/java/com/tyrael/kharazim/demo/NullEqualsNullDemo.java
@@ -0,0 +1,16 @@
+package com.tyrael.kharazim.demo;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2024/3/14
+ */
+@SuppressWarnings("all")
+public class NullEqualsNullDemo {
+
+    public static void main(String[] args) {
+        Object a = null;
+        Object b = null;
+        System.out.println(a == b);
+    }
+
+}
