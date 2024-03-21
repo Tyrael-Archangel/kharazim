bc1737baea245e97691ffb8b4f66a1fd07f7cc1d
Tyrael-Archangel <tyrael_archangel@163.com>
2024-03-21 13:59:04 +0800

    demo 全排列，泛型化

--- kharazim-demo/src/main/java/com/tyrael/kharazim/demo/FullPermutationDemo.java
+++ kharazim-demo/src/main/java/com/tyrael/kharazim/demo/FullPermutationDemo.java
@@ -1,9 +1,10 @@
 package com.tyrael.kharazim.demo;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author Tyrael Archangel
@@ -18,27 +19,51 @@ public class FullPermutationDemo {
         fullPermutation.forEach(System.out::println);
     }
 
-    private static List<List<String>> getFullPermutation(Collection<String> elements) {
+    private static <T> List<List<T>> getFullPermutation(List<T> elements) {
 
-        List<List<String>> results = new ArrayList<>();
-        execute(results, new LinkedHashSet<>(elements), new LinkedHashSet<>());
+        LinkedHashSet<Element<T>> elementSet = new LinkedHashSet<>();
+        int i = 0;
+        for (T element : elements) {
+            elementSet.add(new Element<>(element, i++));
+        }
+
+        List<List<T>> results = new ArrayList<>();
+        execute(results, elementSet, new LinkedHashSet<>());
         return results;
     }
 
     private static <T> void execute(List<List<T>> results,
-                                    LinkedHashSet<T> elements,
-                                    LinkedHashSet<T> usedElements) {
+                                    LinkedHashSet<Element<T>> elements,
+                                    LinkedHashSet<Element<T>> usedElements) {
         if (usedElements.size() == elements.size()) {
-            results.add(new ArrayList<>(usedElements));
+            List<T> result = usedElements.stream()
+                    .map(Element::t)
+                    .collect(Collectors.toList());
+            results.add(result);
             return;
         }
-        for (T element : elements) {
+        for (Element<T> element : elements) {
             if (!usedElements.contains(element)) {
                 usedElements.add(element);
                 execute(results, elements, usedElements);
                 usedElements.remove(element);
             }
         }
+    }
+
+    private record Element<T>(T t, int index) {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Element<?> element)) {
+                return false;
+            }
+            return index == element.index && Objects.equals(t, element.t);
+        }
+
     }
 
 }
