1a8ca9212afdd5119b5682e70e31ce878d4ede52
Tyrael-Archangel <tyrael_archangel@163.com>
2024-04-23 18:06:19 +0800

    mod IndexController

--- kharazim-application/src/main/java/com/tyrael/kharazim/web/controller/system/IndexController.java
+++ kharazim-application/src/main/java/com/tyrael/kharazim/web/controller/system/IndexController.java
@@ -1,8 +1,9 @@
 package com.tyrael.kharazim.web.controller.system;
 
-import com.tyrael.kharazim.common.dto.DataResponse;
 import com.tyrael.kharazim.common.dto.Response;
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Value("${spring.profiles.active:default}")
+    private String profile;
+
     @RequestMapping("/")
     public Response index() {
         return this.bootstrap();
     }
 
     @RequestMapping("/bootstrap")
-    public Response bootstrap() {
-        return DataResponse.ok("Welcome!");
+    public ProfileResponse bootstrap() {
+        return new ProfileResponse(profile);
+    }
+
+    @Data
+    public static class ProfileResponse extends Response {
+
+        private final String profile;
+
+        public ProfileResponse(String profile) {
+            this.profile = profile;
+            markSuccess();
+        }
     }
 
 }
