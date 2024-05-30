a940df9b476fc44b8537eaa09f28c43883d0b235
Tyrael-Archangel <tyrael_archangel@163.com>
2024-05-30 10:53:14 +0800

    add unit test: concurrent next code generate

--- kharazim-application/src/test/java/com/tyrael/kharazim/application/system/service/CodeGeneratorTest.java
+++ kharazim-application/src/test/java/com/tyrael/kharazim/application/system/service/CodeGeneratorTest.java
@@ -4,6 +4,11 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.*;
+
 import static com.tyrael.kharazim.application.config.BusinessCodeConstants.SYSTEM_TEST;
 
 /**
@@ -23,6 +28,39 @@ class CodeGeneratorTest {
         for (int i = 0; i < 10; i++) {
             String next = codeGenerator.next(SYSTEM_TEST);
             System.out.println(next);
+        }
+    }
+
+    @Test
+    void concurrentNext() throws Exception {
+
+        int threadCount = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        try {
+            for (int i = 0; i < 200; i++) {
+                CountDownLatch cdl = new CountDownLatch(threadCount);
+                CyclicBarrier cyclicBarrier = new CyclicBarrier(threadCount);
+                List<String> codes = new ArrayList<>();
+                for (int j = 0; j < threadCount; j++) {
+                    executorService.execute(() -> {
+                        try {
+                            cyclicBarrier.await();
+                            String next = codeGenerator.next(SYSTEM_TEST);
+                            codes.add(next);
+                            cdl.countDown();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+                cdl.await();
+                System.out.println(codes);
+                if (codes.size() != new HashSet<>(codes).size()) {
+                    throw new IllegalStateException("error");
+                }
+            }
+        } finally {
+            executorService.shutdown();
         }
     }
 
