d9420fd3179ec66a8a5ff5832883860093c7c8b9
Tyrael-Archangel <tyrael_archangel@163.com>
2025-03-06 17:25:43 +0800

    reorganize dependency: add lib-discovery-nacos

---
+++ kharazim-lib/kharazim-lib-discovery-redis/src/main/java/com/tyrael/kharazim/discovery/redis/discovery/RedisDiscoveryAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.tyrael.kharazim.discovery.redis.discovery;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2025/3/6
+ */
+public class RedisDiscoveryAutoConfiguration {
+}
---
+++ kharazim-lib/kharazim-lib-discovery-redis/src/main/java/com/tyrael/kharazim/discovery/redis/registry/RedisRegistryAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.tyrael.kharazim.discovery.redis.registry;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2025/3/6
+ */
+public class RedisRegistryAutoConfiguration {
+}
