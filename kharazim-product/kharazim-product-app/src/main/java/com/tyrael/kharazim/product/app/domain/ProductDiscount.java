8bf9b062048e90b140dbe82bc75226102f39fe71
Tyrael-Archangel <tyrael_archangel@163.com>
2025-06-04 17:15:30 +0800

    add product discount

---
+++ kharazim-product/kharazim-product-app/src/main/java/com/tyrael/kharazim/product/app/domain/ProductDiscount.java
@@ -0,0 +1,34 @@
+package com.tyrael.kharazim.product.app.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.tyrael.kharazim.mybatis.BaseDO;
+import com.tyrael.kharazim.product.app.enums.DiscountType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2025/5/26
+ */
+@Data
+@TableName(autoResultMap = true)
+public class ProductDiscount extends BaseDO {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private LocalDateTime effectBegin;
+
+    private LocalDateTime effectEnd;
+
+    private BigDecimal value;
+
+    private DiscountType valueType;
+
+    private Boolean enabled;
+
+}
---
+++ kharazim-product/kharazim-product-app/src/main/java/com/tyrael/kharazim/product/app/enums/DiscountType.java
@@ -0,0 +1,12 @@
+package com.tyrael.kharazim.product.app.enums;
+
+/**
+ * @author Tyrael Archangel
+ * @since 2025/6/4
+ */
+public enum DiscountType {
+
+    FIX_AMOUNT,
+    PERCENTAGE
+
+}
